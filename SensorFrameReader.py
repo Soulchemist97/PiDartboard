67fc79b073a1c0ab24feb00aa28829760a90aad3
Soulchemist97 <Soulchemist97@users.noreply.github.com>
2022-04-29 18:58:38 +0200

    GPIO Readout as Class

---
+++ SensorFrameReader.py
@@ -0,0 +1,78 @@
+import datetime as dt
+# import RPIO
+import RPi.GPIO as GPIO
+
+#Raspberry Pi Pins
+freeGPIOSenderPins = [11,12,13,15,16,18,22,29]
+freeGPIOListenerPins = [31,32,33,35,36,37,38,40]
+
+GPIO.setmode(GPIO.BOARD)
+
+
+def setupListerPins():
+        for pin in freeGPIOListenerPins:
+            GPIO.cleanup(pin)
+            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+
+class SensorFrame():
+    
+    name = "Frame"
+    referenceMatrix = []
+
+    def setName(self,Name):
+        self.name = Name
+        return self.name
+
+    def CurrentTime(self):
+        """Save Current time and date to str
+
+        Returns:
+            currentTime (str): Current time as str
+        """
+        self.currentTime = dt.datetime.now()
+        self.currentTime_str = self.currentTime.strftime("%H:%M:%S %d.%m.%Y")
+        return self.currentTime_str
+    
+    def CaptureReference(self):
+        self.referenceMatrix = [GPIO.input(pin) for pin in freeGPIOListenerPins]
+        return self.referenceMatrix
+
+    def CaptureListenerMatrix(self):
+        self.listenerMatrix = [GPIO.input(pin) for pin in freeGPIOListenerPins]
+        return self.listenerMatrix
+
+    def CaptureFrame(self):
+        pass
+    
+    def Detect(self):
+        setupListerPins()
+        self.CaptureReference()
+        while True:
+            self.CaptureListenerMatrix()
+            
+            if self.referenceMatrix != self.listenerMatrix:
+                
+                print(self.listenerMatrix)
+
+                for pin in freeGPIOSenderPins:
+                    GPIO.output(pin, GPIO.LOW) 
+                    GPIO.output(pin, GPIO.HIGH)
+
+                    senderMatrix = [GPIO.input(pin) for pin in freeGPIOListenerPins]
+
+                    if self.referenceMatrix != senderMatrix:
+                        print(f'Sender matrix for {self.name}: {senderMatrix}')
+                    else:
+                        senderMatrix = []
+                break
+        # return (ListenerPin,SenderPin)
+
+    def main():
+        pass
+
+
+
+if __name__ == '__main__':
+    pass
