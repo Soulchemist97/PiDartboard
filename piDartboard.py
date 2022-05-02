fc1374184822445c307250ecd5976676a4e33f71
Limn0 <67125885+ThisLimn0@users.noreply.github.com>
2022-05-02 13:50:32 +0200

    Add files via upload

---
+++ piDartboard.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+__doc__ = "piDartboard: Main bootstrapper for the piDartboard project."
+__author__ = "ThisLimn0, Soulchemist97"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "ThisLimn0, Soulchemist97"
+__status__ = "Development"
+
+from piDartboardRuntime import *
+
+###TODO###TODO###TODO###TODO###TODO##############################
+## Run the piDartboardRuntime.py file as a daemon and check for crashes.
+## If a crash occurs, restart the daemon.
+
+
+
+
+
